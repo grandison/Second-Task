cb7cd73ded6899671ce832bd8cd4ab30b3a60cd4
Osmanov Alexander <grandison@mail.ru>
2011-05-27 00:13:27 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsSite::Application.config.secret_token = 'e5bc26039ec329eb172353b11256029c63fc046098862bb90917658412397f39a637cc6fb5848fc9cd7b87ac767753d93ce85a6ad048399a37dd7925d3628137'
