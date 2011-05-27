55c626a18b2626dc36d14cb1661057f25cb83197
Osmanov Alexander <grandison@mail.ru>
2011-05-27 20:38:03 +0400

    first part of User model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+# Schema version: 20110527100430
+#
+# Table name: categories
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Category < ActiveRecord::Base
   has_many :news
 end
