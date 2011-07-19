7491f9c625740c210ab5b2bd44bf6dc27ca7f88d
Osmanov Alexander <grandison@mail.ru>
2011-07-19 21:13:10 +0400

    tag search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
     @posts= @posts.topic(params[:topic]) if params[:topic].present?
     @posts= @posts.text(params[:text]) if params[:text].present?
     @posts= @posts.cat_id(params[:category_id]) if params[:category_id].present?
+    @posts= @posts.tagged_with(params[:post][:tag_list]) if params[:post].present?&&params[:post][:tag_list].present?
     if params[:order].present?
       @posts = @posts.order("rating #{params[:order]}")
     else
